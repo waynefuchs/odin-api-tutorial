f34f149c8c83e07edb6d45fd73c51abd9c2b772d
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-05-13 02:35:41 -0400

    Complete Appliaction Level Express Middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import { v4 as uuidv4 } from "uuid";
 import cors from "cors";
 import express from "express";
 
@@ -27,9 +28,20 @@ let messages = {
 };
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
+////////////////////////////////////////////////////////////////////////////////
+// MIDDLEWARE
+
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
+
+////////////////////////////////////////////////////////////////////////////////
 // USERS
 
 app.get("/users", (req, res) => {
@@ -52,7 +64,9 @@ app.delete("/users/:userId", (req, res) => {
   return res.send(`DELETE HTTP method on users/${req.params.userId} resource`);
 });
 
+////////////////////////////////////////////////////////////////////////////////
 // MESSAGES
+
 app.get("/messages", (req, res) => {
   return res.json(Object.values(messages));
 });
@@ -61,6 +75,25 @@ app.get("/messages/:userId", (req, res) => {
   return res.json(messages[req.params.userId]);
 });
 
+app.post("/messages", (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+  messages[id] = message;
+  return res.send(message);
+});
+
+app.delete("/messages/:messageId", (req, res) => {
+  const { [req.params.messageId]: messageToDelete, ...otherMessages } =
+    messages;
+  messages = otherMessages;
+  return res.send(messageToDelete);
+});
+
+////////////////////////////////////////////////////////////////////////////////
 // SERVER START
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
